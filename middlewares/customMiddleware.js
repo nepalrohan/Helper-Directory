39c7800b31b48e03c0fb52d7c42b619d8ce4033a
nepalrohan <rohannepal098@gmail.com>
2025-03-15 21:21:47 +0545

    add custom middlewares

---
+++ middlewares/customMiddleware.js
@@ -0,0 +1,18 @@
+
+export const requestLogger = (req, res, next)=>{
+    const timeStamp = new Date().toISOString();
+    const method = req.method;
+    const url = req.url;
+    const userAgent = req.get('User-Agent');
+
+    console.log(`[${timeStamp}] ${method} ${url}- ${userAgent}`);
+    next();
+}
+
+
+
+
+ export const addTimeStamp = (req, res, next)=>{
+    req.timeStamp = new Date().toISOString();
+    next();
+}
--- server.js
+++ server.js
@@ -3,13 +3,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { configureCors } from './config/cors-config.js';
+import { addTimeStamp, requestLogger } from './middlewares/customMiddleware.js';
 const app = express();
 
 
 app.use(configureCors());
+app.use(requestLogger());
+app.use(addTimeStamp());
 
 const PORT = process.env.PORT || 8000;
 app.use(express.json())
+
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server started at port:${PORT}`)
